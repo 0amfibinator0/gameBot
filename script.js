57cc6d6ecf6728f49b7ded2b03da387befa23474
0amfibinator0 <00amfibiantor00@mail.ru>
2021-11-06 21:35:55 +0300

    усложненка 6 урока сдеана

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 'use strict';
 
-const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-};
-
 function one (x) {
     x = Math.floor(Math.random( ) * (100 - 1 + 1)) + 1;
-    function two () {
+    function two (i = 10) {
         let a = prompt('Угадай число от 1 до 100');
-        if (a == null) {
+
+        if (a === null) {
             alert('Игра окончена');
         }else if (a < x && a !== null) {
-            alert('Загаданное число больше');
-            two();
+            if (i > 0) {
+                alert(`Загаданное число больше, осталось попыток ${i - 1}`);
+                two(i - 1);
+            } else {
+                if (confirm ('Попытки закончились, хотите сыграть еще?')) {
+                    two();
+                } else {
+                    alert ('Всего хорошего!');
+                }
+            }
         } else if (a > x && a !== null) {
-            alert('Загаданное число меньше');
-            two();
+            if (i > 0) {
+                alert(`Загаданное число меньше, осталось попыток ${i - 1}`);
+                two(i - 1);
+            } else {
+                if (confirm ('Попытки закончились, хотите сыграть еще?')) {
+                    two();
+                } else {
+                    alert ('Всего хорошего!');
+                }
+            }
         } else if (a == x && a !== null) {
-            alert('Поздравляю, Вы угадали!!!');
-        } else if (!isNumber(a) && a !== null) {
+            if (confirm ('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+                two();
+            } else {
+                alert ('Всего хорошего!');
+            }
+        } else if (a !== /[^\d]/) {
             alert('Введи число!');
-            two();
+            two(i);
         }
     }
     two();
